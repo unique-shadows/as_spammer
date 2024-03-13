79e6fdd1cd39d24ad9d487ccd188f58c714530da
Unique Shadows <149162089+unique-shadows@users.noreply.github.com>
2024-03-14 02:57:58 +0530

    Update data_spam.py

--- data_spam.py
+++ data_spam.py
@@ -1,5 +1,15 @@
 from requests import get
 
+proxies = {
+    'http': 'http://gofw8axb8qtmdr:gdmfjuci61ip31n7oghrct6w1nq@proxy.quotaguard.com:9292',
+    'https': 'http://gofw8axb8qtmdr:gdmfjuci61ip31n7oghrct6w1nq@proxy.quotaguard.com:9292'
+}
+
 while True:
-    a = get('https://user-verify-pearl.vercel.app/pwhls?link=Abhi data chor hai, sudhar jaa bhai, tere bhale ke liye kah rha hun')
-    print(a.status_code)
+    a = get(
+        'https://user-verify-pearl.vercel.app/pwhls?link=Abhi data chor hai, sudhar jaa bhai, tere bhale ke liye kah rha hun', 
+        proxies=proxies
+        
+        )
+    
+    print(a.json(), a.status_code)
