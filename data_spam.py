067800e343643b7b0f49da7a43449e80034b3129
Unique Shadows <149162089+unique-shadows@users.noreply.github.com>
2024-03-17 21:15:07 +0530

    Update data_spam.py

--- data_spam.py
+++ data_spam.py
@@ -1,14 +1,14 @@
 from requests import get
 
 proxies = {
-    'http': 'http://pz600g667zu7ad:9i21feayawlhh3rlbz6i9ea53vck79@proxy.quotaguard.com:9292',
-    'https': 'http://pz600g667zu7ad:9i21feayawlhh3rlbz6i9ea53vck79@proxy.quotaguard.com:9292'
+    'http': 'http://zhl4dg0w7a8h4s:4kgbdnzin4qb1cl86fzi82tn6olvgm@proxy.quotaguard.com:9292',
+    'https': 'http://zhl4dg0w7a8h4s:4kgbdnzin4qb1cl86fzi82tn6olvgm@proxy.quotaguard.com:9292'
 }
 
 while True:
     a = get(
-        'https://api.alphastudyofficial.live/alpha/pw-khazana/get/lectures?khazana_slug=11th-jee-khazana-607507&subject_slug=complete-mathematics---11th-845097&teacher_slug=complete-mathematics-by-253144&chapter_slug=62cc8cd39f43b7001293d1c0'
-        # proxies=proxies
+        'https://api.alphastudyofficial.live/alpha/pw-khazana/get/lectures?khazana_slug=11th-jee-khazana-607507&subject_slug=complete-mathematics---11th-845097&teacher_slug=complete-mathematics-by-253144&chapter_slug=62cc8cd39f43b7001293d1c0',
+        proxies=proxies
         
         )
     
